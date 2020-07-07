7d43f94296d674dc355464bfdf65ca87230536a7
Gar <gar@lonelydatu.com>
2020-07-07 16:48:06 -0400

    one

--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -1,6 +1,36 @@
 
 
+function start(){
+	TweenLite.defaultEase = Power3.easeInOut
+	const tl = new TimelineMax()
+	
+	tl.set(".frame1", {opacity:1})
+	tl.from(".t1_a", .01, {opacity:0}, "+=.2")
+	tl.from([".t1_b", ".t1_c"], .01, {opacity:0}, "+=.7")
+	
 
+	tl.from(".hero_a", .6, {opacity:.8}, 0)
+
+	tl.add("f2", 3.1)
+	tl.to(".frame1", .3, {opacity:0}, "f2")
+	tl.set(".frame2", {opacity:1}, "f2")
+
+	tl.add("phone_move")
+	tl.from('.phone', .4, {y:"+=100", ease:Power3.easeOut}, "phone_move")
+	tl.from('.illust', .3, {scale:0}, "phone_move+=.3")
+
+	tl.from('.t2', .01, {opacity:0}, "+=.3")
+	tl.from('.cta', .1, {opacity:0}, "+=1.4")
+	tl.to('.cta', .2, {opacity:0}, "+=2.3")
+	tl.from('.cta2', .3, {opacity:0})
+	
+
+	tl.from('.legal', .3, {opacity:0}, "+=.1")
+
+	// tl.gotoAndPlay("f2")
+}
+
+start()
 
 module.exports = {};
 
