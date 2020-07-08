1b3c01b79ae0b141860b87aab98810eeeb9e667d
Gar <gar@lonelydatu.com>
2020-07-08 12:20:24 -0400

    on

--- dev/std_300x250/_bundled/log-free.js
+++ dev/std_300x250/_bundled/log-free.js
@@ -3,18 +3,18 @@
 
 function batter() {
 	var tl = new TimelineMax();
-	TweenLite.to(".hero_batter_bg", .3, { opacity: 1 });
-	TweenLite.from([".batter_all"], 2, { x: "+=6", ease: Power2.easeOut });
+	TweenLite.to(".hero_batter_bg", .5, { opacity: 1 });
+	TweenLite.from([".batter_all"], 2, { x: "+=5", ease: Power2.easeOut });
 	TweenLite.from([".ball"], 1, { x: "-=20", ease: Power2.easeOut });
 	var list = [".hero_batter_1", ".hero_batter_3", ".hero_batter_5", ".hero_batter_7", ".hero_batter_9"];
 	list.map(function (item, index) {
 
-		var time = "+=" + index * .022;
+		var time = "-=" + index * .01;
 		var notIt = list.filter(function (f) {
 			return f !== item;
 		});
 		tl.add("frame", time);
-		tl.set(notIt, { opacity: 0 }, "frame");
+		tl.to(notIt, .15, { opacity: 0 }, "frame");
 		tl.set(item, { opacity: 1 }, "frame");
 	});
 
